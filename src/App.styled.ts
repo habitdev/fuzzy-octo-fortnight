25b0cc467b53e09ed0cadb2d8f8cb34821505e9e
habitdev <goodstory16@gmail.com>
2025-08-27 20:33:14 +0900

    Add: update styling for Hero sections and gsap animation

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -2,245 +2,332 @@ import styled from 'styled-components';
 
 // Layout
 export const Main = styled.main`
-  width: 100%;
+	width: 100%;
+`;
+
+export const Container = styled.div`
+	padding: 0 40px;
 `;
 
 // Visual Section
-export const VisualSection = styled.section`
-  overflow: hidden;
-  position: relative;
-  height: 100vh;
+export const HeroSection = styled.section`
+	background-color: #111;
 `;
 
+export const HeroSectionAll = styled.div`
+	position: relative;
+`;
+
+export const HeroWrap = styled.article`
+	position: sticky;
+	top: 0;
+	left: 0;
+	height: 300svh;
+`;
+
+export const HeroTitleWrap = styled.div`
+	position: sticky;
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	align-content: center;
+	justify-content: center;
+	height: 100svh;
+	font-size: 6.875rem;
+	font-weight: 700;
+	line-height: 1;
+	color: #fff;
+	text-align: center;
+`;
 export const HeroTitle = styled.div`
-  z-index: 2;
-  position: absolute;
-  top: 50%;
-  left: 0;
-  right: 0;
-  transform: translateY(-50%);
-  font-size: 110px;
-  font-weight: 700;
-  line-height: 1;
-  color: #fff;
+	opacity: 1;
+	transform: translateY(0);
+	z-index: 2;
+	position: relative;
 `;
 
 export const HeroTitleRow = styled.p`
-  overflow: hidden;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+	overflow: hidden;
+	width: 100%;
 `;
 
 export const HeroSpot = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 200svh;
 `;
 
 export const HeroMask = styled.figure`
-  position: sticky;
-  left: 0;
-  width: 100%;
-  height: 100svh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  clip-path: inset(18% 36%);
+	position: sticky;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100svh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	clip-path: inset(20% 41%);
 `;
 
 export const MaskBg = styled.span`
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 export const MaskImage = styled.img`
-  object-fit: cover;
+	object-fit: cover;
+	width: 100%;
+`;
+
+// Wave section
+export const WaveWrap = styled.article`
+	position: relative;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 300svh;
+	text-align: center; 
+`;
+
+export const WaveWrapInner = styled.div`
+	position: sticky;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%; 
+`;
+
+export const WaveTitWrap = styled.div`
+	position: sticky;
+	top: 0;
+	left: 0;
+	height: 100svh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column; 
+`;
+
+export const WaveTit = styled.h2`
+	opacity: 0;
+	transform: translateY(30px);
+	color: #fff;
+	font-size: 6.875rem;
+	font-weight: 700;
+	z-index: 2;
+	margin-bottom: 2rem;
+`;
+
+export const WaveCircleWrap = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	height: 100svh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const WaveCircles = styled.div`
+	position: relative;
+	width: 80vh;
+	height: 80vh;
+	margin-left: auto;
+	margin-right: auto;
+`;
+
+export const WaveCircle = styled.span`
+	opacity: 0;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 40%;
+	height: 40%;
+	border-radius: 50%;
+	border: 1px solid rgba(255, 255, 255, 0.5);
+	transform: translate(-50%, -50%) scale(0.5);
 `;
 
 // Emotion Section
 export const EmotionSection = styled.section`
-  overflow: hidden;
-  position: relative;
-  padding-top: 6rem;
-  padding-bottom: 10rem;
-  border-top-right-radius: 3.75rem;
-  border-top-left-radius: 3.75rem;
-  background-color: #fff;
-`;
-
-export const Container = styled.div`
-  padding: 0 40px;
+	overflow: hidden;
+	position: relative;
+	margin-top: 0;
+	padding-top: 6rem;
+	padding-bottom: 12rem;
+	border-top-right-radius: 3.75rem;
+	border-top-left-radius: 3.75rem;
+	background-color: #fff;
 `;
 
 export const EmotionTextWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: left;
+	display: flex;
+	justify-content: center;
+	text-align: left;
 `;
 
-export const EmotionText = styled.p<{ space?: boolean }>`
-  font-size: 60px;
-  ${({ space }) => space && 'margin-left: 13vw;'}
+export const EmotionText = styled.p.withConfig({
+	shouldForwardProp: (prop) => prop !== 'space',
+})<{ space?: boolean }>`
+	font-size: 3.75rem;
+	${({ space }) => space && 'margin-left: 13vw;'}
 
-  strong {
-    font-weight: 800;
-  }
+	strong {
+		font-weight: 800;
+	}
 `;
 
 // About Section
 export const AboutSection = styled.section`
-  position: relative;
-  overflow: hidden;
+	position: relative;
+	overflow: hidden;
 `;
 
 export const AboutCirclesWrap = styled.div`
-  position: sticky;
-  display: flex;
-  justify-content: flex-end;
+	position: sticky;
+	display: flex;
+	justify-content: flex-end;
 `;
 
 export const AboutCircles = styled.ul`
-  display: grid;
-  width: 180vh;
-  height: 120vh;
-  margin-top: 12rem;
-  margin-right: -90vh;
-  grid-template-rows: 60vh 60vh;
-  grid-template-columns: repeat(3, 60vh);
-  grid-auto-columns: 1fr;
+	display: grid;
+	grid-template-rows: 60vh 60vh;
+	grid-template-columns: repeat(3, 60vh);
+	grid-auto-columns: 1fr;
+	width: 180vh;
+	height: 120vh;
+	margin-top: 12rem;
+	margin-right: -90vh;
 `;
 
+export const AboutCirclesItem = styled.li``;
+
 export const Circle = styled.div`
-  width: 60vh;
-  height: 60vh;
-  border-radius: 50%;
-  padding: 3.75rem;
-  border: 1px solid #dcdcdc;
+	width: 60vh;
+	height: 60vh;
+	border-radius: 50%;
+	padding: 3.75rem;
+	border: 1px solid #dcdcdc;
 `;
 
 export const CircleObj = styled.figure`
-  position: relative;
-  text-align: center;
+	position: relative;
+	text-align: center;
 `;
 
 export const CircleImage = styled.img`
-  border-radius: 50%;
-  width: 100%;
+	border-radius: 50%;
+	width: 100%;
 `;
 
 export const CircleTxt = styled.figcaption`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 3.125rem;
-  font-weight: 600;
-  color: #fff;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 3.125rem;
+	font-weight: 600;
+	color: #fff;
 `;
 
 // About Background
 export const AboutBg = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  text-align: center;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	text-align: center;
 `;
 
 export const BgListWrap = styled.div`
-  position: sticky;
-  width: 100%;
-  height: 100%;
+	position: sticky;
+	width: 100%;
+	height: 100%;
 `;
 
 export const BgList = styled.ul`
-  display: inline-grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 2rem;
-  padding-top: 10rem;
+	display: inline-grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: 2rem;
+	padding-top: 10rem;
 `;
 
 export const BgListItem = styled.li<{ position?: string }>`
-  display: flex;
-  ${({ position }) =>
-    position === 'top' && 'align-items: flex-end;'}
-  ${({ position }) =>
-    position === 'left' && 'justify-content: flex-end;'}
+	display: flex;
+	${({ position }) => position === 'top' && 'align-items: flex-end;'}
+	${({ position }) => position === 'left' && 'justify-content: flex-end;'}
 
   &.left.top {
-    transform: translate(-100%, -100%);
-  }
-  &.right.top {
-    transform: translate(100%, -100%);
-  }
-  &.left.bottom {
-    transform: translate(-100%, 100%);
-  }
-  &.right.bottom {
-    transform: translate(100%, 100%);
-  }
+		transform: translate(-100%, -100%);
+	}
+	&.right.top {
+		transform: translate(100%, -100%);
+	}
+	&.left.bottom {
+		transform: translate(-100%, 100%);
+	}
+	&.right.bottom {
+		transform: translate(100%, 100%);
+	}
 `;
 
 export const BgImage = styled.img`
-  opacity: 0.3;
-  filter: blur(8px);
+	opacity: 0.3;
+	filter: blur(8px);
 `;
 
 // About Words
 export const AboutWords = styled.ul`
-  font-size: 60px;
-  font-weight: 500;
-  color: #fff;
-  text-align: center;
+	font-size: 60px;
+	font-weight: 500;
+	color: #fff;
+	text-align: center;
 `;
 
 export const AboutWordItem = styled.li`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
 `;
 
 export const AboutText = styled.p`
-  strong {
-    font-weight: 600;
-  }
+	strong {
+		font-weight: 600;
+	}
 `;
 
 // Footer
 export const Footer = styled.footer`
-  padding-top: 3rem;
-  padding-bottom: 3rem;
-  border-top-right-radius: 3.75rem;
-  border-top-left-radius: 3.75rem;
-  background-color: #5b5bf1;
+	padding-top: 3rem;
+	padding-bottom: 3rem;
+	border-top-right-radius: 3.75rem;
+	border-top-left-radius: 3.75rem;
+	background-color: #5b5bf1;
 `;
 
 export const BtnLink = styled.a`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  padding: 0.625em 1em;
-  color: #fff;
-  font-size: 50px;
-  text-decoration: none;
-  border: 2px solid #fff;
-  border-radius: 2.125rem;
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	padding: 0.625em 1em;
+	color: #fff;
+	font-size: 50px;
+	text-decoration: none;
+	border: 2px solid #fff;
+	border-radius: 2.125rem;
 
-  span + img {
-    margin-left: 0.5em;
-  }
+	span + img {
+		margin-left: 0.5em;
+	}
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,178 +1,340 @@
 import { Link } from 'react-router-dom';
 import {
-  AboutBg,
-  AboutCircles,
-  AboutCirclesWrap,
-  AboutSection,
-  AboutText,
-  AboutWordItem,
-  AboutWords,
-  BgImage,
-  BgList,
-  BtnLink,
-  Circle,
-  CircleImage,
-  CircleObj,
-  CircleTxt,
-  Container,
-  EmotionSection,
-  EmotionText,
-  EmotionTextWrap,
-  Footer,
-  HeroMask,
-  HeroSpot,
-  HeroTitle,
-  HeroTitleRow,
-  Main,
-  MaskBg,
-  MaskImage,
-  VisualSection
+	AboutBg,
+	AboutCircles,
+	AboutCirclesItem,
+	AboutCirclesWrap,
+	AboutSection,
+	AboutText,
+	AboutWordItem,
+	AboutWords,
+	BgImage,
+	BgList,
+	BtnLink,
+	Circle,
+	CircleImage,
+	CircleObj,
+	CircleTxt,
+	Container,
+	EmotionSection,
+	EmotionText,
+	EmotionTextWrap,
+	Footer,
+	HeroMask,
+	HeroSection,
+	HeroSectionAll,
+	HeroSpot,
+	HeroTitle,
+	HeroTitleRow,
+	HeroTitleWrap,
+	HeroWrap,
+	Main,
+	MaskBg,
+	MaskImage,
+	WaveCircle,
+	WaveCircles,
+	WaveCircleWrap,
+	WaveTit,
+	WaveTitWrap,
+	WaveWrap,
+	WaveWrapInner,
 } from './App.styled';
 
+import gsap from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from 'react';
+
 function App() {
-  return (
-    <>
-      <Main>
-        <VisualSection>
-          <HeroTitle>
-            <HeroTitleRow>
-              <span>Your Mood,</span>
-            </HeroTitleRow>
-            <HeroTitleRow>
-              <span>Your Music.</span>
-            </HeroTitleRow>
-          </HeroTitle>
+	gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-          <HeroSpot>
-            <HeroMask>
-              <MaskBg>
-                <MaskImage src="/assets/img_hero.jpg" alt="cloud" />
-              </MaskBg>
-            </HeroMask>
-          </HeroSpot>
-        </VisualSection>
+	const heroRef = useRef<HTMLElement | null>(null);
+	const heroBgWrapRef = useRef(null);
+	const heroBgRef = useRef(null);
+	const heroSectionRef = useRef<HTMLElement | null>(null);
+	const heroAllRef = useRef<HTMLDivElement | null>(null);
+	const heroTitWrapRef = useRef(null);
+	const heroTitRef = useRef(null);
+	const waveSectionRef = useRef<HTMLElement | null>(null);
+	const waveTitWrapRef = useRef(null);
+	const waveTitRef = useRef(null);
+	const rippleRef = useRef(null);
+	const emotionRef = useRef(null);
+	const aboutRef = useRef(null);
+	const footerRef = useRef(null);
 
-        <EmotionSection>
-          <Container>
-            <EmotionTextWrap>
-              <div className="emotion-words">
-                <EmotionText>
-                  Some <strong>feelings</strong> can’t be explained.
-                </EmotionText>
-                <EmotionText space>
-                  But they can be <strong>heard</strong>.
-                </EmotionText>
-                <EmotionText>
-                  We help you <strong>hear</strong> yours.
-                </EmotionText>
-              </div>
-            </EmotionTextWrap>
+	useEffect(() => {
+		// Hero clip-path
 
-            <AboutCirclesWrap>
-              <AboutCircles>
-                <li className="about-circles__item">
-                  <Circle>
-                    <CircleObj>
-                      <CircleImage src="/assets/bg_circle_obj_happy.png" alt="" />
-                      <CircleTxt>Happy</CircleTxt>
-                    </CircleObj>
-                  </Circle>
-                </li>
-                <li className="about-circles__item">
-                  <Circle>
-                    <CircleObj>
-                      <CircleImage src="/assets/bg_circle_obj_calm.png" alt="" />
-                      <CircleTxt>Calm</CircleTxt>
-                    </CircleObj>
-                  </Circle>
-                </li>
-                <li className="about-circles__item">
-                  <Circle>
-                    <CircleObj>
-                      <CircleImage src="/assets/bg_circle_obj_focus.png" alt="" />
-                      <CircleTxt>Focus</CircleTxt>
-                    </CircleObj>
-                  </Circle>
-                </li>
-                <li className="about-circles__item">
-                  <Circle>
-                    <CircleObj>
-                      <CircleImage src="/assets/bg_circle_obj_sad.png" alt="" />
-                      <CircleTxt>Sad</CircleTxt>
-                    </CircleObj>
-                  </Circle>
-                </li>
-                <li className="about-circles__item">
-                  <Circle>
-                    <CircleObj>
-                      <CircleImage src="/assets/bg_circle_obj_lonely.png" alt="" />
-                      <CircleTxt>Lonely</CircleTxt>
-                    </CircleObj>
-                  </Circle>
-                </li>
-                <li className="about-circles__item">
-                  <Circle>
-                    <CircleObj>
-                      <CircleImage src="/assets/bg_circle_obj_dreamy.png" alt="" />
-                      <CircleTxt>Dreamy</CircleTxt>
-                    </CircleObj>
-                  </Circle>
-                </li>
-              </AboutCircles>
-            </AboutCirclesWrap>
-          </Container>
-        </EmotionSection>
+		const gsapContext = gsap.context(() => {
+			// ScrollTrigger.create({
+			// 	trigger: heroSectionRef.current,
+			// 	start: 'top top',
+			// 	end: `bottom bottom`,
+			// 	pin: true,
+			// 	// markers: true
+			// });
 
-        <AboutSection>
-          <AboutBg>
-            <div className="bg-list-wrap">
-              <BgList>
-                <li className="left top">
-                  <BgImage src="/assets/bg_about_obj_01.png" alt="album" />
-                </li>
-                <li className="right top">
-                  <BgImage src="/assets/bg_about_obj_02.png" alt="record" />
-                </li>
-                <li className="left bottom">
-                  <BgImage src="/assets/bg_about_obj_03.png" alt="album" />
-                </li>
-                <li className="right bottom">
-                  <BgImage src="/assets/bg_about_obj_04.png" alt="album" />
-                </li>
-              </BgList>
-            </div>
-          </AboutBg>
-          <Container>
-            <AboutWords>
-              <AboutWordItem>
-                <AboutText>
-                  You <strong>feel</strong> it.
-                </AboutText>
-              </AboutWordItem>
-              <AboutWordItem>
-                <AboutText>
-                  We <strong>play</strong> it.
-                </AboutText>
-              </AboutWordItem>
-              <AboutWordItem>
-                <AboutText>
-                  A playlist born from your <strong>mood</strong>.
-                </AboutText>
-              </AboutWordItem>
-            </AboutWords>
-          </Container>
-        </AboutSection>
-      </Main>
-      <Footer>
-        <Container>
-          <BtnLink as={Link} to="#">
-            <span>Wanna give it a try?</span>
-            <img src="/assets/ico_arrow_down_left.svg" alt="arrow" />
-          </BtnLink>
-        </Container>
-      </Footer>
-    </>
-  );
+			ScrollTrigger.defaults({
+				// markers: true,
+				// fast scroll에서 문제 추적
+			});
+
+			gsap.to(heroBgRef.current, {
+				id: 'hero-bg',
+				clipPath: 'inset(-20% -30%)',
+				opacity: 0,
+				scrollTrigger: {
+					trigger: heroBgWrapRef.current,
+					start: 'top top',
+					end: `center top`,
+					scrub: true,
+					// markers: true,
+				},
+			});
+
+			gsap.to(heroTitRef.current, {
+				id: 'hero-tit',
+				opacity: 0,
+				y: -30,
+				scrollTrigger: {
+					trigger: heroBgWrapRef.current,
+					start: 'center+=10% top',
+					end: 'center+=10% top',
+					toggleActions: 'play none reverse none',
+					// markers: true,
+				},
+			});
+
+			gsap.to(waveTitRef.current, {
+				id: 'wave-tit',
+				opacity: 1,
+				y: 0,
+				duration: 0.5,
+				scrollTrigger: {
+					trigger: waveTitWrapRef.current,
+					start: 'center top',
+					end: `center top`,
+					toggleActions: 'play none reverse none',
+					// markers: true,
+				},
+			});
+
+			ScrollTrigger.create({
+				trigger: waveSectionRef.current,
+				start: 'top top',
+				end: `bottom top`,
+				pin: true,
+				pinSpacing: false,
+				// markers: true,
+			});
+
+			const q = gsap.utils.selector(rippleRef);
+			const circles = q('.circle');
+
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: rippleRef.current,
+					start: 'center center',
+					end: 'bottom center',
+					scrub: true,
+					markers: true,
+				},
+			});
+
+			circles.forEach((circle, index) => {
+				tl.fromTo(
+					circle,
+					{
+						scale: 0,
+						opacity: 0,
+					},
+					{
+						scale: 8,
+						opacity: 0,
+						duration: 1.5,
+						ease: 'power1.out',
+						keyframes: [
+							{ opacity: 1, duration: 0.2 },
+							{ opacity: 0, duration: 0.5 },
+						],
+					},
+					index * 0.5
+				);
+			});
+		});
+
+
+
+		setTimeout(() => {
+			ScrollTrigger.refresh();
+		}, 100);
+
+		return () => gsapContext.revert();
+	}, []);
+
+	return (
+		<>
+			<Main>
+				<HeroSection ref={heroSectionRef}>
+					<HeroSectionAll ref={heroAllRef}>
+						<HeroWrap ref={heroRef}>
+							<HeroSpot ref={heroBgWrapRef}>
+								<HeroMask ref={heroBgRef}>
+									<MaskBg>
+										<MaskImage src="/assets/img_hero.jpg" alt="cloud" />
+									</MaskBg>
+								</HeroMask>
+							</HeroSpot>
+
+							<HeroTitleWrap ref={heroTitWrapRef}>
+								<HeroTitle ref={heroTitRef}>
+									<HeroTitleRow>Your Mood,</HeroTitleRow>
+									<HeroTitleRow>Your Music.</HeroTitleRow>
+								</HeroTitle>
+							</HeroTitleWrap>
+						</HeroWrap>
+
+						<WaveWrap ref={waveSectionRef}>
+							<WaveWrapInner ref={rippleRef}>
+								<WaveTitWrap ref={waveTitWrapRef}>
+									<WaveTit ref={waveTitRef}>Your feeling is…</WaveTit>
+								</WaveTitWrap>
+								<WaveCircleWrap>
+									<WaveCircles>
+										<WaveCircle className="circle" />
+										<WaveCircle className="circle" />
+										<WaveCircle className="circle" />
+										<WaveCircle className="circle" />
+										<WaveCircle className="circle" />
+									</WaveCircles>
+								</WaveCircleWrap>
+							</WaveWrapInner>
+						</WaveWrap>
+					</HeroSectionAll>
+				</HeroSection>
+
+				<EmotionSection ref={emotionRef}>
+					<Container>
+						<EmotionTextWrap>
+							<div className="emotion-words">
+								<EmotionText>
+									Some <strong>feelings</strong> can’t be explained.
+								</EmotionText>
+								<EmotionText space={true}>
+									But they can be <strong>heard</strong>.
+								</EmotionText>
+								<EmotionText>
+									We help you <strong>hear</strong> yours.
+								</EmotionText>
+							</div>
+						</EmotionTextWrap>
+
+						<AboutCirclesWrap>
+							<AboutCircles>
+								<AboutCirclesItem>
+									<Circle>
+										<CircleObj>
+											<CircleImage src="/assets/bg_circle_obj_happy.png" alt="" />
+											<CircleTxt>Happy</CircleTxt>
+										</CircleObj>
+									</Circle>
+								</AboutCirclesItem>
+								<AboutCirclesItem>
+									<Circle>
+										<CircleObj>
+											<CircleImage src="/assets/bg_circle_obj_calm.png" alt="" />
+											<CircleTxt>Calm</CircleTxt>
+										</CircleObj>
+									</Circle>
+								</AboutCirclesItem>
+								<AboutCirclesItem>
+									<Circle>
+										<CircleObj>
+											<CircleImage src="/assets/bg_circle_obj_focus.png" alt="" />
+											<CircleTxt>Focus</CircleTxt>
+										</CircleObj>
+									</Circle>
+								</AboutCirclesItem>
+								<AboutCirclesItem>
+									<Circle>
+										<CircleObj>
+											<CircleImage src="/assets/bg_circle_obj_sad.png" alt="" />
+											<CircleTxt>Sad</CircleTxt>
+										</CircleObj>
+									</Circle>
+								</AboutCirclesItem>
+								<AboutCirclesItem>
+									<Circle>
+										<CircleObj>
+											<CircleImage src="/assets/bg_circle_obj_lonely.png" alt="" />
+											<CircleTxt>Lonely</CircleTxt>
+										</CircleObj>
+									</Circle>
+								</AboutCirclesItem>
+								<AboutCirclesItem>
+									<Circle>
+										<CircleObj>
+											<CircleImage src="/assets/bg_circle_obj_dreamy.png" alt="" />
+											<CircleTxt>Dreamy</CircleTxt>
+										</CircleObj>
+									</Circle>
+								</AboutCirclesItem>
+							</AboutCircles>
+						</AboutCirclesWrap>
+					</Container>
+				</EmotionSection>
+
+				<AboutSection>
+					<AboutBg>
+						<div className="bg-list-wrap">
+							<BgList>
+								<li className="left top">
+									<BgImage src="/assets/bg_about_obj_01.png" alt="album" />
+								</li>
+								<li className="right top">
+									<BgImage src="/assets/bg_about_obj_02.png" alt="record" />
+								</li>
+								<li className="left bottom">
+									<BgImage src="/assets/bg_about_obj_03.png" alt="album" />
+								</li>
+								<li className="right bottom">
+									<BgImage src="/assets/bg_about_obj_04.png" alt="album" />
+								</li>
+							</BgList>
+						</div>
+					</AboutBg>
+					<Container>
+						<AboutWords>
+							<AboutWordItem>
+								<AboutText>
+									You <strong>feel</strong> it.
+								</AboutText>
+							</AboutWordItem>
+							<AboutWordItem>
+								<AboutText>
+									We <strong>play</strong> it.
+								</AboutText>
+							</AboutWordItem>
+							<AboutWordItem>
+								<AboutText>
+									A playlist born from your <strong>mood</strong>.
+								</AboutText>
+							</AboutWordItem>
+						</AboutWords>
+					</Container>
+				</AboutSection>
+			</Main>
+			<Footer>
+				<Container>
+					<BtnLink as={Link} to="#">
+						<span>Wanna give it a try?</span>
+						<img src="/assets/ico_arrow_down_left.svg" alt="arrow" />
+					</BtnLink>
+				</Container>
+			</Footer>
+		</>
+	);
 }
 
 export default App;
